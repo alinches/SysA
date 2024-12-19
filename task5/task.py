0305afb51e465260a527010766fcb41cb33dfd65
alinches <53570305+alinches@users.noreply.github.com>
2024-12-19 10:31:05 +0300

    Update task.py

--- task5/task.py
+++ task5/task.py
@@ -1,82 +1,117 @@
 import json
-import numpy as np
 
-def create_relation_matrix(ranking_json: str):
-    """Создает матрицу отношений из ранжирования."""
-    ranking_groups = [group if isinstance(group, list) else [group] for group in json.loads(ranking_json)]
-    size = sum(len(group) for group in ranking_groups)
-    # Создание базовой матрицы отношений
-    relation_matrix = [[1] * size for _ in range(size)]
+def main(varA: str, varB: str) -> str:
+    rankingA = json.loads(varA)
+    rankingB = json.loads(varB)
     
-    previous_elements = []
-    for group in ranking_groups:
-        for prev_elem in previous_elements:
-            for curr_elem in group:
-                relation_matrix[curr_elem - 1][prev_elem - 1] = 0
-        for elem in group:
-            previous_elements.append(int(elem))
-
-    return np.array(relation_matrix)
-
-def determine_clusters(relation_matrix, matrix_a, matrix_b):
-    """Определяет кластеры на основе матрицы отношений."""
-    clusters = {}
-    total_rows = len(relation_matrix)
-    processed_rows = set()
-
-    # Объединение элементов в кластеры
-    for row in range(total_rows):
-        if row + 1 in processed_rows:
-            continue
-        new_cluster = [row + 1]
-        clusters[row + 1] = new_cluster
-        for col in range(row + 1, total_rows):
-            if relation_matrix[row][col] == 0:
-                new_cluster.append(col + 1)
-                processed_rows.add(col + 1)
-
-    # Формирование итогового списка кластеров
-    final_clusters = []
-    for key in clusters:
-        if not final_clusters:
-            final_clusters.append(clusters[key])
-            continue
+    def extract_objects(ranking):
+        result = []
+        for elem in ranking:
+            if isinstance(elem, list):
+                for x in elem:
+                    result.append(x)
+            else:
+                result.append(elem)
+        return result
+    
+    objsA = set(extract_objects(rankingA))
+    objsB = set(extract_objects(rankingB))
+    if objsA != objsB:
+        raise ValueError("The two rankings do not have the same set of objects.")
+    
+    all_objs = sorted(list(objsA))
+    index = {obj: i for i, obj in enumerate(all_objs)}
+    
+    def build_position_map(ranking):
+        pos_map = {}
+        current_rank = 0
+        for elem in ranking:
+            if isinstance(elem, list):
+                for x in elem:
+                    pos_map[x] = current_rank
+                current_rank += 1
+            else:
+                pos_map[elem] = current_rank
+                current_rank += 1
+        return pos_map
+    
+    posA = build_position_map(rankingA)
+    posB = build_position_map(rankingB)
+    
+    n = len(all_objs)
+    Y_A = [[0]*n for _ in range(n)]
+    Y_B = [[0]*n for _ in range(n)]
+    
+    for i_obj in all_objs:
+        for j_obj in all_objs:
+            i_idx = index[i_obj]
+            j_idx = index[j_obj]
+            # Для A
+            if posA[j_obj] <= posA[i_obj]:
+                Y_A[i_idx][j_idx] = 1
+            else:
+                Y_A[i_idx][j_idx] = 0
             
-        for i, cluster in enumerate(final_clusters):
-            sum_a_cluster = np.sum(matrix_a[cluster[0] - 1])
-            sum_b_cluster = np.sum(matrix_b[cluster[0] - 1])
-            sum_a_key = np.sum(matrix_a[key - 1])
-            sum_b_key = np.sum(matrix_b[key - 1])
+            # Для B
+            if posB[j_obj] <= posB[i_obj]:
+                Y_B[i_idx][j_idx] = 1
+            else:
+                Y_B[i_idx][j_idx] = 0
+    
+    def transpose(M):
+        return list(map(list, zip(*M)))
+    
+    Y_A_T = transpose(Y_A)
+    Y_B_T = transpose(Y_B)
+    
+    def mat_and(M1, M2):
+        return [[1 if (M1[i][j] == 1 and M2[i][j] == 1) else 0 for j in range(n)] for i in range(n)]
+    
+    def mat_or(M1, M2):
+        return [[1 if (M1[i][j] == 1 or M2[i][j] == 1) else 0 for j in range(n)] for i in range(n)]
+    
+    Y_AB = mat_and(Y_A, Y_B)
+    Y_AB_prime = mat_and(Y_A_T, Y_B_T)
+    Y_final = mat_or(Y_AB, Y_AB_prime)
+    
+    from collections import defaultdict, deque
+    graph = defaultdict(list)
+    for i_obj in range(n):
+        for j_obj in range(i_obj+1, n):
+            if Y_final[i_obj][j_obj] == 0 and Y_final[j_obj][i_obj] == 0:
+                graph[i_obj].append(j_obj)
+                graph[j_obj].append(i_obj)
+    
+    visited = [False]*n
+    def bfs(start):
+        q = deque([start])
+        visited[start] = True
+        comp = [all_objs[start]]
+        while q:
+            u = q.popleft()
+            for w in graph[u]:
+                if not visited[w]:
+                    visited[w] = True
+                    comp.append(all_objs[w])
+                    q.append(w)
+        return comp
+    
+    kernels = []
+    for i in range(n):
+        if not visited[i] and i in graph:
+            comp = bfs(i)
+            if len(comp) == 1:
+                continue
+            comp.sort()
+            kernels.append(comp)
+    
+    if not kernels:
+        kernels = []
+    
+    return json.dumps(kernels, ensure_ascii=False)
 
-            if sum_a_cluster == sum_a_key and sum_b_cluster == sum_b_key:
-                for member in clusters[key]:
-                    final_clusters[i].append(member)
-                    break
-            elif sum_a_cluster < sum_a_key or sum_b_cluster < sum_b_key:
-                final_clusters = final_clusters[:i] + clusters[key] + final_clusters[i:]
-                break
-
-        final_clusters.append(clusters[key])
-
-    return [cluster[0] if len(cluster) == 1 else cluster for cluster in final_clusters]
-
-def task():
-    """Основная функция для обработки ранжирования."""
-    ranking_a = '[1,[2,3],4,[5,6,7],8,9,10]'
-    ranking_b = '[[1,2],[3,4,5],6,7,9,[8,10]]'
-
-    matrix_a = create_relation_matrix(ranking_a)
-    matrix_b = create_relation_matrix(ranking_b)
-
-    # Пересечение и объединение матриц
-    combined_matrix = np.multiply(matrix_a, matrix_b)
-    combined_transposed = np.multiply(np.transpose(matrix_a), np.transpose(matrix_b))
-    merged_matrix = np.maximum(combined_matrix, combined_transposed)
-
-    # Определение кластеров
-    result_clusters = determine_clusters(merged_matrix, matrix_a, matrix_b)
-    return json.dumps(result_clusters)
 
 if __name__ == "__main__":
-    print(task())
-
+    A_json = '[1,[2,3],4,[5,6,7],8,9,10]'
+    B_json = '[[1,2],[3,4,5],6,7,9,[8,10]]'
+    print(main(A_json, B_json))
